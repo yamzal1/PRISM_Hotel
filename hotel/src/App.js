62551d30439f382f86568a26f887fa6374861543
Azdarhanos <darkphenix03@gmail.com>
2022-06-09 12:33:53 +0200

    Formulaire de paiement

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -9,6 +9,7 @@ import Erreur from "./pages/Erreur";
 import { Route, Switch } from 'react-router-dom'
 
 import Navigation from "./components/Navigation";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route exact path="/" component={ PageAccueil } />
       <Route exact path="/chambres/" component={ Chambres } />
       <Route exact path="/chambres/:slug" component={ ChambreSeule } />
+      <Route exact path="/checkout/" component={ Checkout } />
       <Route component={ Erreur } />
     </Switch>
     </>
