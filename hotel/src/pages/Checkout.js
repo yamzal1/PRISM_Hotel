967b2cc6083b70360ad51f2da9b77fc5fc744aea
Jules <jules.comb@gmail.com>
2022-06-13 14:38:49 +0000

    do stuff

--- hotel/src/pages/Checkout.js
+++ hotel/src/pages/Checkout.js
@@ -16,7 +16,61 @@ const Checkout = (props) => {
 	const date = location.state.date.value.toString()
 	const dateM = date.split(',')
 	const dateDeb = dateM[0].split('GMT')
-	const dateFin = dateM[0].split('GMT')
+	const dateFin = dateM[1].split('GMT')
+
+    function reserveRoom() {
+        console.log("ruby")
+
+        var slug = "yannis";
+        var headers = new Headers();
+        headers.append("cache-control", "no-cache");
+        headers.append("content-type", "application/json")
+        headers.append("x-apikey", "62348bc0dced170e8c83a37c");
+
+        fetch("https://pommedeterre-20df.restdb.io/rest/chambre", {
+            method: 'GET',
+            headers: headers,
+            mode: 'cors',
+            cache: 'default'
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let selectedRoom;
+                    result.some(function (room) {
+                        if (room.slug == slug) {
+                            selectedRoom = room;
+                            return true;
+                        }
+                    });
+
+                    console.log("debut :", dateDeb);
+                    console.log("fin :", dateFin);
+
+                    selectedRoom.reserved = true;
+                    selectedRoom.begin_date = dateDeb;
+                    selectedRoom.end_date = dateFin;
+
+                    console.log(selectedRoom);
+
+                    fetch("https://pommedeterre-20df.restdb.io/rest/chambre", {
+                        method: 'POST',
+                        headers: headers,
+                        mode: 'cors',
+                        cache: 'default',
+                        body: JSON.stringify(selectedRoom)
+                    })
+                        .then(
+                            (error) => {
+                                console.log(error)
+                            }
+                        )
+                },
+                (error) => {
+                    console.log(error)
+                }
+            )
+    }
 
     const notify = () => toast.success("Paiement effectué ! Vous allez recevoir un mail", {
         position: "top-center",
@@ -38,62 +92,13 @@ const Checkout = (props) => {
                 </Banniere>
             </Hero>
             <div className="centrer">
-                <Link onClick={() => {
-                    var slug = "yannis";
-                    var headers = new Headers();
-                    headers.append("cache-control", "no-cache");
-                    headers.append("content-type", "application/json")
-                    headers.append("x-apikey", "62348bc0dced170e8c83a37c");
-
-                    fetch("https://pommedeterre-20df.restdb.io/rest/chambre", {
-                        method: 'GET',
-                        headers: headers,
-                        mode: 'cors',
-                        cache: 'default'
-                    })
-                        .then(res => res.json())
-                        .then(
-                            (result) => {
-                                let selectedRoom;
-                                result.some(function (room) {
-                                    if (room.slug == slug) {
-                                        selectedRoom = room;
-                                        return true;
-                                    }
-                                });
-
-                                console.log("hello", dateDeb);
-
-                                selectedRoom.reserved = true;
-                                selectedRoom.begin_date = dateDeb;
-                                selectedRoom.end_date = dateFin;
-
-                                console.log(selectedRoom);
-
-                                fetch("https://pommedeterre-20df.restdb.io/rest/chambre", {
-                                    method: 'POST',
-                                    headers: headers,
-                                    mode: 'cors',
-                                    cache: 'default',
-                                    body: JSON.stringify(selectedRoom)
-                                })
-                                    .then(
-                                        (error) => {
-                                            console.log(error)
-                                        }
-                                    )
-                            },
-                            (error) => {
-                                console.log(error)
-                            }
-                        )
-                }} className="btn-primary">
+                <button onClick={reserveRoom} className="btn-primary">
                     Reserver la chambre
-                </Link>
+                </button>
                 <MyCards />
                 <button onClick={notify} className="btn-primary btn-payer">
                     Payer
-        </button>
+                </button>
                 <ToastContainer />
             </div>
         </>
