52ff5ea6c8f891db547be7538a31b04a564fedd1
ElodieGRR <97110472+ElodieGRR@users.noreply.github.com>
2022-06-13 21:17:45 +0200

    modifs frontend contact et btn payer

--- hotel/src/components/Formulaire.js
+++ hotel/src/components/Formulaire.js
@@ -89,9 +89,10 @@ class Formulaire extends React.Component {
             <div class="mb-3 pt-0">
                 <textarea
                     required
+                    rows={1}
                     placeholder="Nom"
                     type="text"
-                    class="m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
+                    class="resize-none m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
                                         name="nom"
                     value={nom}
                     onChange={e => this.setState({
@@ -105,12 +106,13 @@ class Formulaire extends React.Component {
                  <textarea
             
                         required
+                        rows={1}
                         data-type="email"   
                         type="email"  
                         placeholder="E-mail"       
                         name="email"
                         value={email}
-                        class="m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
+                        class="resize-none m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
                                                 onChange={e => this.setState({
                           email: e.target.value
                         })}
@@ -120,12 +122,13 @@ class Formulaire extends React.Component {
             <div class="mb-3 pt-0">
             <textarea
                         required
+                        rows={1}
                         data-type="objet"   
                         type="objet"
                         placeholder="Objet"         
                         name="objet"
                         value={objet}
-                        class="m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
+                        class="resize-none m-2 form-flex-1 appearance-none border border-[#dcbb92] w-full py-2 px-4 bg-white text-[#dcbb92] placeholder-[#dcbb92] rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-[#dcbb92] focus:border-transparent" 
                                                 onChange={e => this.setState({
                           objet: e.target.value
                         })}
@@ -144,7 +147,7 @@ class Formulaire extends React.Component {
 
             </div >
             <div class="mb-3 pt-0">
-            <button type="submit" class="ml-48 justify-center py-2 px-4 bg-[#dcbb92] text-white w-1/2 duration-200 text-center font-semibold rounded-lg ">Envoyer</button>
+            <button type="submit" class="ml-44 justify-center py-2 px-4 bg-[#dcbb92] text-white w-1/2 duration-200 text-center font-semibold rounded-lg ">Envoyer</button>
             </div>
           </form>
           </div>
@@ -153,8 +156,8 @@ class Formulaire extends React.Component {
                     <p class="font-bold text-center text-xl"> Notre adresse </p>
                     <p class="text-center"> 13 avenue des Sciences </p>
                     <p class="text-center"> 91190 Gif-Sur-Yvette</p>
-                    <p class="font-bold text-center text-xl">Appelez nous </p>
-                    <p class="text-center mb-4"> +33 1 01 01 01 01</p>
+                    <p class="font-bold text-center text-xl mt-4">Appelez nous </p>
+                    <p class="text-center mb-8"> +33 1 01 01 01 01</p>
                     <div class="flex justify-center ">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.7113352753418!2d2.1686606514238176!3d48.710998118925346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67f538f1df01d%3A0xe5d2ea29a4e0ceb!2sIUT%20d&#39;Orsay!5e0!3m2!1sfr!2sfr!4v1652610451161!5m2!1sfr!2sfr" width="400" height="300" class="w-full b-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
                     </div>
